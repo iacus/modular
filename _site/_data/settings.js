d7f0813740523f798bd37b7025e616167cea65f0
Iago Barreiro <iago.barreiro@gmail.com>
2020-02-01 21:28:18 +0100

    Seo added

--- _site/_data/settings.js
+++ _site/_data/settings.js
@@ -1,5 +1,6 @@
 module.exports = {
    siteName: 'Modular Multiespazo',
+   siteDescription: 'Cultura, coworking e outras cousiñas',
    mail: 'info@somosmodular.com',
    copyright: 'Love Hvar 2019 – Photographs: Daniel Riera'
 }
