48b48d78fb0d598475b91fb976cb5212666b9ed2
iacus <iago.barreiro@gmail.com>
2020-06-03 13:56:12 +0200

    meta description

--- _site/_data/settings.js
+++ _site/_data/settings.js
@@ -1,6 +1,6 @@
 module.exports = {
    siteName: 'Modular Multiespazo',
-   siteDescription: 'Cultura, coworking e outras cousiñas',
+   siteDescription: 'Espacio de coworking en Santiago de Compostela',
    seoDescription: 'Espacio de coworking en Santiago de Compostela con acceso 24/7 para los miembros, fibra óptica de alta velocidad, cocina y terraza con vistas a la catedral.',
    mail: 'info@somosmodular.com'
 }
