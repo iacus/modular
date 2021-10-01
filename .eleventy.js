3f102bbc765c35b579b886cefff3822151c11b12
Iago Barreiro <iago.barreiro@gmail.com>
2021-10-01 16:15:37 +0200

    images optimiced

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,29 @@
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, klass, alt, sizes) {
+  let metadata = await Image(src, {
+    widths: [300, 600, 1000, 1500],
+    formats: ["webp", "png", "jpeg"],
+    outputDir: "./dist/images",
+    urlPath: "/images/",
+    useCache: true
+  });
+
+  let imageAttributes = {
+    class: klass,
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
+
+  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+  return Image.generateHTML(metadata, imageAttributes);
+}
+
 module.exports = function (config) {
+
+  config.addNunjucksAsyncShortcode("image", imageShortcode);
 
    config.addCollection('posts', collection => {
       return collection.getFilteredByGlob('_site/posts/*.md')
